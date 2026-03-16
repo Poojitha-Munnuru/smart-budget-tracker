fad339d59f622b081afd0e39c7ab47a0cb149274
Poojitha-Munnuru <23r21a12g6@mlrit.ac.in>
2026-03-16 23:06:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let balance = document.getElementById("balance");
+let income = document.getElementById("income");
+let expense = document.getElementById("expense");
+let list = document.getElementById("list");
+
+let total = 0;
+let inc = 0;
+let exp = 0;
+
+function addTransaction() {
+
+let text = document.getElementById("text").value;
+let amount = Number(document.getElementById("amount").value);
+
+if(text === "" || amount === 0){
+alert("Enter description and amount");
+return;
+}
+
+let li = document.createElement("li");
+li.innerText = text + " : ₹" + amount;
+list.appendChild(li);
+
+if(amount > 0){
+inc += amount;
+}else{
+exp += Math.abs(amount);
+}
+
+total = inc - exp;
+
+balance.innerText = "₹" + total;
+income.innerText = "₹" + inc;
+expense.innerText = "₹" + exp;
+
+document.getElementById("text").value = "";
+document.getElementById("amount").value = "";
+}
