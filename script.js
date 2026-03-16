240296fae3d9667a5180f125d68801c6b4a3e997
Poojitha-Munnuru <23r21a12g6@mlrit.ac.in>
2026-03-16 21:07:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,60 @@
 const balance = document.getElementById("balance");
-const money_plus = document.getElementById("money-plus");
-const money_minus = document.getElementById("money-minus");
-const form = document.getElementById("form");
+const income = document.getElementById("income");
+const expense = document.getElementById("expense");
 const list = document.getElementById("list");
 
-form.addEventListener("submit", addTransaction);
+let transactions = [];
 
-function addTransaction(e) {
-  e.preventDefault();
+function addTransaction(){
 
-  const text = document.getElementById("text").value;
-  const amount = +document.getElementById("amount").value;
+const text = document.getElementById("text").value;
+const amount = parseInt(document.getElementById("amount").value);
 
-  const li = document.createElement("li");
-  li.innerHTML = `${text} <span>${amount}</span>`;
+if(text === "" || isNaN(amount)){
+alert("Enter description and amount");
+return;
+}
 
-  list.appendChild(li);
+const transaction = {
+text:text,
+amount:amount
+};
+
+transactions.push(transaction);
+
+const li = document.createElement("li");
+
+li.innerHTML = `${text} : ₹${amount}`;
+
+list.appendChild(li);
+
+updateValues();
+
+document.getElementById("text").value="";
+document.getElementById("amount").value="";
+
+}
+
+function updateValues(){
+
+let total=0;
+let inc=0;
+let exp=0;
+
+transactions.forEach(t=>{
+
+total += t.amount;
+
+if(t.amount>0){
+inc += t.amount;
+}else{
+exp += t.amount;
+}
+
+});
+
+balance.innerText="₹"+total;
+income.innerText="₹"+inc;
+expense.innerText="₹"+Math.abs(exp);
+
 }
